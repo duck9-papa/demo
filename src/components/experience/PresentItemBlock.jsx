614997b24ab901cade9f04a448625687b23ea399
inho <nanaheui000@naver.com>
2024-12-20 22:40:17 +0900

    경로 수정

--- src/components/experience/PresentItemBlock.jsx
+++ src/components/experience/PresentItemBlock.jsx
@@ -1,9 +1,9 @@
 import { useInView } from "react-intersection-observer";
-import {
-  PresentContent,
-  PresentItem,
-  PresentItemTitle,
-} from "/src/assets/styles/CommonStyled";
+// import {
+//   PresentContent,
+//   PresentItem,
+//   PresentItemTitle,
+// } from "/src/assets/styles/CommonStyled";
 import { useEffect, useState } from "react";
 
 const PresentItemBlock = ({ presentItem }) => {
@@ -15,20 +15,21 @@ const PresentItemBlock = ({ presentItem }) => {
   }, [inView]);
 
   return (
-    <PresentItem ref={ref} $on={first}>
-      <PresentItemTitle>
-        <h2>{presentItem.title}</h2>
-        <span>{presentItem.detail}</span>
-      </PresentItemTitle>
-      {presentItem.content.map((content, contentIndex) => (
-        <PresentContent key={`contentKey${contentIndex}`}>
-          <div className="title">{`${contentIndex + 1}. ${content.title}`}</div>
-          {content.details.map((detail, detailIndex) => (
-            <span key={`detailKey${detailIndex}`}>- {detail}</span>
-          ))}
-        </PresentContent>
-      ))}
-    </PresentItem>
+    <></>
+    // <PresentItem ref={ref} $on={first}>
+    //   <PresentItemTitle>
+    //     <h2>{presentItem.title}</h2>
+    //     <span>{presentItem.detail}</span>
+    //   </PresentItemTitle>
+    //   {presentItem.content.map((content, contentIndex) => (
+    //     <PresentContent key={`contentKey${contentIndex}`}>
+    //       <div className="title">{`${contentIndex + 1}. ${content.title}`}</div>
+    //       {content.details.map((detail, detailIndex) => (
+    //         <span key={`detailKey${detailIndex}`}>- {detail}</span>
+    //       ))}
+    //     </PresentContent>
+    //   ))}
+    // </PresentItem>
   );
 };
 
